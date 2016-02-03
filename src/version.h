bbf0c06bdc120f76f0b38a0eca3d339aaa7d425a
ambiprofin <ambiprofin@users.noreply.github.com>
2016-02-03 23:50:12 +0530

    Change Version and Block Old Peers

--- src/version.h
+++ src/version.h
@@ -24,19 +24,20 @@ extern const std::string CLIENT_DATE;
 //
 // database format versioning
 //
+
 static const int DATABASE_VERSION = 70509;
 
 //
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 60020;
+static const int PROTOCOL_VERSION = 70010;
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 60020;
+static const int MIN_PEER_PROTO_VERSION = 70000;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
