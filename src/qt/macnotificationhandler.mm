60a530fec22caffb879d688433cb56351c1ff6a1
konez2k <32417933+konez2k@users.noreply.github.com>
2017-10-09 10:13:23 +0200

    Fixed name

--- src/qt/macnotificationhandler.mm
+++ src/qt/macnotificationhandler.mm
@@ -13,7 +13,7 @@
 - (NSString *)__bundleIdentifier
 {
     if (self == [NSBundle mainBundle]) {
-        return @"org.hyperstake.HyperStake-Qt";
+        return @"info.buzzcoin.BuzzCoin-Qt";
     } else {
         return [self __bundleIdentifier];
     }
