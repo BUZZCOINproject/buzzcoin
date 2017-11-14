babbf563d074e4f4e95915c0fcac1fb43baf03f2
awitherow <au.witherow@gmail.com>
2017-11-14 16:30:28 +0100

    add ability to modify walletdb with split threshold

--- src/walletdb.cpp
+++ src/walletdb.cpp
@@ -117,6 +117,12 @@ bool CWalletDB::WriteOrderPosNext(int64_t nOrderPosNext)
     return Write(std::string("orderposnext"), nOrderPosNext);
 }
 
+bool CWalletDB::WriteStakeSplitThreshold(uint64_t nStakeSplitThreshold)
+{
+    nWalletDBUpdated++;
+    return Write(std::string("stakeSplitThreshold"), nStakeSplitThreshold);
+}
+
 bool CWalletDB::WriteDefaultKey(const CPubKey &vchPubKey)
 {
     nWalletDBUpdated++;
@@ -545,6 +551,10 @@ bool ReadKeyValue(CWallet *pwallet, CDataStream &ssKey, CDataStream &ssValue,
         else if (strType == "orderposnext")
         {
             ssValue >> pwallet->nOrderPosNext;
+        }
+        else if (strType == "stakeSplitThreshold")
+        {
+            ssValue >> pwallet->nStakeSplitThreshold;
         }
     }
     catch (...)
--- src/walletdb.h
+++ src/walletdb.h
@@ -37,8 +37,8 @@ enum DBErrors
 
 class CKeyMetadata
 {
-public:
-    static const int CURRENT_VERSION=1;
+  public:
+    static const int CURRENT_VERSION = 1;
     int nVersion;
     int64_t nCreateTime; // 0 means unknown
 
@@ -52,12 +52,10 @@ public:
         nCreateTime = nCreateTime_;
     }
 
-    IMPLEMENT_SERIALIZE
-    (
+    IMPLEMENT_SERIALIZE(
         READWRITE(this->nVersion);
         nVersion = this->nVersion;
-        READWRITE(nCreateTime);
-    )
+        READWRITE(nCreateTime);)
 
     void SetNull()
     {
@@ -66,59 +64,64 @@ public:
     }
 };
 
-
 /** Access to the wallet database (wallet.dat) */
 class CWalletDB : public CDB
 {
-public:
-    CWalletDB(const std::string& strFilename, const char* pszMode = "r+") : CDB(strFilename, pszMode)
+  public:
+    CWalletDB(const std::string &strFilename, const char *pszMode = "r+") : CDB(strFilename, pszMode)
     {
     }
-private:
-    CWalletDB(const CWalletDB&);
-    void operator=(const CWalletDB&);
-public:
-    bool WriteName(const std::string& strAddress, const std::string& strName);
 
-    bool EraseName(const std::string& strAddress);
+  private:
+    CWalletDB(const CWalletDB &);
+    void operator=(const CWalletDB &);
 
-    bool WriteTx(uint256 hash, const CWalletTx& wtx);
+  public:
+    bool WriteName(const std::string &strAddress, const std::string &strName);
+
+    bool EraseName(const std::string &strAddress);
+
+    bool WriteTx(uint256 hash, const CWalletTx &wtx);
     bool EraseTx(uint256 hash);
 
-    bool WriteKey(const CPubKey& vchPubKey, const CPrivKey& vchPrivKey, const CKeyMetadata &keyMeta);
-    bool WriteCryptedKey(const CPubKey& vchPubKey, const std::vector<unsigned char>& vchCryptedSecret, const CKeyMetadata &keyMeta);
-    bool WriteMasterKey(unsigned int nID, const CMasterKey& kMasterKey);
+    bool WriteKey(const CPubKey &vchPubKey, const CPrivKey &vchPrivKey, const CKeyMetadata &keyMeta);
+    bool WriteCryptedKey(const CPubKey &vchPubKey, const std::vector<unsigned char> &vchCryptedSecret, const CKeyMetadata &keyMeta);
+    bool WriteMasterKey(unsigned int nID, const CMasterKey &kMasterKey);
 
-    bool WriteCScript(const uint160& hash, const CScript& redeemScript);
+    bool WriteCScript(const uint160 &hash, const CScript &redeemScript);
 
-    bool WriteBestBlock(const CBlockLocator& locator);
-    bool ReadBestBlock(CBlockLocator& locator);
+    bool WriteBestBlock(const CBlockLocator &locator);
+    bool ReadBestBlock(CBlockLocator &locator);
 
     bool WriteOrderPosNext(int64_t nOrderPosNext);
 
-    bool WriteDefaultKey(const CPubKey& vchPubKey);
+    bool WriteStakeSplitThreshold(uint64_t nStakeSplitThreshold);
 
-    bool ReadPool(int64_t nPool, CKeyPool& keypool);
-    bool WritePool(int64_t nPool, const CKeyPool& keypool);
+    bool WriteDefaultKey(const CPubKey &vchPubKey);
+
+    bool ReadPool(int64_t nPool, CKeyPool &keypool);
+    bool WritePool(int64_t nPool, const CKeyPool &keypool);
     bool ErasePool(int64_t nPool);
 
     bool WriteMinVersion(int nVersion);
 
-    bool ReadAccount(const std::string& strAccount, CAccount& account);
-    bool WriteAccount(const std::string& strAccount, const CAccount& account);
-private:
-    bool WriteAccountingEntry(const uint64_t nAccEntryNum, const CAccountingEntry& acentry);
-public:
-    bool WriteAccountingEntry(const CAccountingEntry& acentry);
-    int64_t GetAccountCreditDebit(const std::string& strAccount);
-    void ListAccountCreditDebit(const std::string& strAccount, std::list<CAccountingEntry>& acentries);
+    bool ReadAccount(const std::string &strAccount, CAccount &account);
+    bool WriteAccount(const std::string &strAccount, const CAccount &account);
 
-    DBErrors ReorderTransactions(CWallet*);
-    DBErrors LoadWallet(CWallet* pwallet);
-    static bool Recover(CDBEnv& dbenv, std::string filename, bool fOnlyKeys);
-    static bool Recover(CDBEnv& dbenv, std::string filename);
+  private:
+    bool WriteAccountingEntry(const uint64_t nAccEntryNum, const CAccountingEntry &acentry);
+
+  public:
+    bool WriteAccountingEntry(const CAccountingEntry &acentry);
+    int64_t GetAccountCreditDebit(const std::string &strAccount);
+    void ListAccountCreditDebit(const std::string &strAccount, std::list<CAccountingEntry> &acentries);
+
+    DBErrors ReorderTransactions(CWallet *);
+    DBErrors LoadWallet(CWallet *pwallet);
+    static bool Recover(CDBEnv &dbenv, std::string filename, bool fOnlyKeys);
+    static bool Recover(CDBEnv &dbenv, std::string filename);
 };
 
-bool BackupWallet(const CWallet& wallet, const std::string& strDest);
+bool BackupWallet(const CWallet &wallet, const std::string &strDest);
 
 #endif // BITCOIN_WALLETDB_H
