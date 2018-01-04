b5792c258eb0c8994d3c1326a967fbaceeef900f
awitherow <au.witherow@gmail.com>
2018-01-04 12:57:12 -0500

    ensure testnet can stake4chartiy

--- src/main.cpp
+++ src/main.cpp
@@ -2362,8 +2362,8 @@ bool ProcessBlock(CNode* pfrom, CBlock* pblock)
 
     LogPrintf("ProcessBlock: ACCEPTED\n");
 
-	// If turned on stakeforcharity, send a portion of stake reward to savings account address
-	if (pwalletMain->fStakeForCharity && mapBlockIndex[hash]->nHeight < Params().StabilitySoftFork())
+	// If turned on stake4charity, send a portion of stake reward to savings account address
+	if (pwalletMain->fStakeForCharity && (mapBlockIndex[hash]->nHeight >= Params().StabilitySoftFork() || TestNet()))
 		if (!pwalletMain->StakeForCharity())
 			LogPrint("s4c", "ERROR While trying to send portion of stake reward to savings account");
 
