992c31db63f1989429de7a4dce51d621ba65238c
awitherow <au.witherow@gmail.com>
2018-01-11 12:20:49 -0500

    change mining difficulty

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -132,7 +132,7 @@ public:
         pchMessageStart[1] = 0x22;
         pchMessageStart[2] = 0x05;
         pchMessageStart[3] = 0x30;
-        bnProofOfWorkLimit = CBigNum(~uint256(0) >> 16);
+        bnProofOfWorkLimit = CBigNum(~uint256(0) >> 1);
         vAlertPubKey = ParseHex("04a983220ea7a38a7106385003fef77896538a382a0dcc389cc45f3c98751d9af423a097789757556259351198a8aaa628a1fd644c3232678c5845384c744ff8d7");
         nDefaultPort = 20114;
         nRPCPort = 20115;
