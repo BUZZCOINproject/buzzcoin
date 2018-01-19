9c4888097bad931492de88c423a2588e5f6ba0f4
awitherow <au.witherow@gmail.com>
2018-01-19 15:50:26 -0500

    update to 3.0

--- src/qt/locale/bitcoin_ur_PK.ts
+++ src/qt/locale/bitcoin_ur_PK.ts
@@ -1,4 +1,4 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="ur_PK" version="2.1">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="ur_PK" version="3.0">
 <context>
     <name>AboutDialog</name>
     <message>
