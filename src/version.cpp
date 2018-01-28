db87b3a90a105324318577e4e8d8640aebeef5a7
Tonda Atouk <tonda.atouk@gmail.com>
2018-01-28 15:07:44 +0100

    removed git flag

--- src/version.cpp
+++ src/version.cpp
@@ -34,7 +34,7 @@ const std::string CLIENT_NAME("BUZZ");
 #endif
 
 // git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
-#define GIT_ARCHIVE 1
+//#define GIT_ARCHIVE 1
 //#ifdef GIT_ARCHIVE
 //#    define GIT_COMMIT_ID "32a928e"
 //#    define GIT_COMMIT_DATE "$Format:%cD"
