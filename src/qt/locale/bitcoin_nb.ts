f3d201aaff79d7fa48757eade5e819f762b3bb9e
awitherow <au.witherow@gmail.com>
2018-02-04 14:12:51 -0500

    add locale version updates

--- src/qt/locale/bitcoin_nb.ts
+++ src/qt/locale/bitcoin_nb.ts
@@ -1,4 +1,4 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="nb" version="3.1">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="nb" version="3.1.1">
 <context>
     <name>AboutDialog</name>
     <message>
--- src/qt/locale/bitcoin_sk.ts
+++ src/qt/locale/bitcoin_sk.ts
@@ -1,4 +1,4 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="sk" version="3.1">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="sk" version="3.1.1">
 <context>
     <name>AboutDialog</name>
     <message>
